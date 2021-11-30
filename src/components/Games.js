57cab2b61908d6cc4dd8dbf42e11bf8ac5219874
Stu Bolderson <stu.bolderson@aol.com>
2021-11-30 12:31:41 +0100

    popular games is fetching data apply same logic to upcoming games and test rendering the data in jsx

--- src/components/Games.js
+++ src/components/Games.js
@@ -1,6 +1,14 @@
-import React from "react";
+import React, { useEffect } from "react";
+import { useDispatch } from "react-redux";
+import { loadGames } from "../actions/gamesAction";
 
 export const Games = () => {
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    dispatch(loadGames());
+  }, [dispatch]);
+
   return (
     <div>
       <h1>Hello </h1>
